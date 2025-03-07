abb133f8415c1e0eae60739b686d84233db99b51
Yernur <erkeerke349@gmail.com>
2025-03-07 18:20:46 +0500

    config

--- config/ConfigManagerDemo.java
+++ config/ConfigManagerDemo.java
@@ -1,4 +1,18 @@
 package config;
 
 public class ConfigManagerDemo {
+    public static void main(String[] args) {
+        ConfigurationManager configurationManager = ConfigurationManager.getInstance();
+
+
+        System.out.println("individual config values:");
+        System.out.println("max players: " + configurationManager.getConfig("maxPlayers"));
+        System.out.println("default language: " + configurationManager.getConfig("defaultLanguage"));
+        System.out.println("game difficulty: " + configurationManager.getConfig("gameDifficulty"));
+
+        System.out.println();
+
+        System.out.println("all config:");
+        configurationManager.printAllConfig();
+    }
 }
--- config/ConfigurationManager.java
+++ config/ConfigurationManager.java
@@ -1,4 +1,32 @@
 package config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConfigurationManager {
+    private static ConfigurationManager instance;
+    private final Map<String, String> config = new HashMap<>();
+
+    private ConfigurationManager() {
+        config.put("maxPlayers", "100");
+        config.put("defaultLanguage", "en");
+        config.put("gameDifficulty", "medium");
+    }
+
+    public static ConfigurationManager getInstance() {
+        if (instance == null) {
+            instance = new ConfigurationManager();
+        }
+        return instance;
+    }
+
+    public String getConfig(String key) {
+        return config.getOrDefault(key, "not found");
+    }
+
+    public void printAllConfig() {
+        for (Map.Entry<String, String> entry : config.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
 }
